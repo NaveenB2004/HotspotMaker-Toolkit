23a62e8b81cc8f8760ac5b69f33a784ee60c7367
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2023-08-10 16:18:31 +0530

    init project

---
+++ src/main/java/Main/HotspotMakerToolkit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author NaveenB2004
+ */
+public class HotspotMakerToolkit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
